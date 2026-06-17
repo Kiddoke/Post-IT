7c1aad0b475137aa274aca8405a5ac531aa72a2f
Kevin Lu <kevil@uio.no>
2026-06-17 19:20:18 +0200

    Initial commmit with SQL model

---
+++ post/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    tittel = models.CharField(max_length=20)
+    innhold = models.TextField()
+    status = models.CharField(max_length=10, default='ny')
+    opprettet = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tittel
